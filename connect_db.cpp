134b12c2b63d28aeafedef85af4ed330484fc270
Mi <harold.y.mi@outlook.com>
2022-10-09 04:35:56 +0800

    0.0.1

--- connect_db.cpp
+++ connect_db.cpp
@@ -10,11 +10,12 @@ static bool createConnection(){
     db.setDatabaseName("dc_db");
     db.setUserName("root");
     db.setPassword("17312767927");
-    bool ok = db.open();
+    // bool ok = db.open();
     if (!db.open()) {
         QMessageBox::critical(0, QObject::tr("无法打开数据库"),"无法创建数据库连接！ ", QMessageBox::Cancel);
         return false;
         }
-    else return true;
+    else{
+        return true;
     }
 }
