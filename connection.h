9d95ed29ab0a98e8523479c010d6fde17fdee590
Mi <harold.y.mi@outlook.com>
2022-10-09 02:30:13 +0800

    alpha make

---
+++ connection.h
@@ -0,0 +1,12 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+#include <QMessageBox>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+//用来连接数据库
+
+static bool createConnection()
+{
+
+}
+#endif // CONNECTION_H
