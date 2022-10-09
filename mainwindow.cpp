84b6b72036a00609e09542ee83d1114db1d9e1a6
Mi <harold.y.mi@outlook.com>
2022-10-09 15:13:43 +0800

    0.0.3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,9 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "show_db.h"
-
+#include <QVideoWidget>
+#include <QMediaPlayer>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,28 +13,50 @@ MainWindow::MainWindow(QWidget *parent)
 
     createConnection();
 
+    //set column name
     QSqlTableModel *model = new QSqlTableModel;
         model->setTable("nonstd");
         model->setEditStrategy(QSqlTableModel::OnManualSubmit);
         model->select();
-        model->setHeaderData(0, Qt::Horizontal, tr("分析角度"));
-        model->setHeaderData(1, Qt::Horizontal, tr("记录视频"));
+        model->setHeaderData(1, Qt::Horizontal, tr("分析角度"));
+        model->setHeaderData(2, Qt::Horizontal, tr("记录视频"));
+        model->setHeaderData(7, Qt::Horizontal, tr("动作标准度"));
 
-
+    //set column to show
     QTableView *view = ui->tableView;
-        view->hideColumn(0);
-        view->setColumnHidden(3,true);
-        view->setColumnHidden(4,true);
-        view->setColumnHidden(5,true);
-        view->setColumnHidden(6,true);
-        view->setColumnHidden(7,true);
-        view->setColumnHidden(8,true);
-        view->setColumnHidden(9,true);
-        view->setColumnHidden(10,true);
-        view->setColumnHidden(11,true);
-        view->setColumnHidden(12,true);
         view->setModel(model);
+
+        view->hideColumn(0);    //name
+        view->hideColumn(1);    //angle
+        view->hideColumn(2);    //video
+        view->hideColumn(3);    //o_video
+        view->hideColumn(4);    //p_back
+        view->hideColumn(5);    //p_front
+        view->hideColumn(6);    //p_high
+        // view->hideColumn(7);   //std
+        view->hideColumn(8);   //j_ru
+        view->hideColumn(9);   //j_chu
+        view->hideColumn(10);   //j_high
+        view->hideColumn(11);   //p_left_wrist
+
         view->show();
+
+    //play video
+    QMediaPlayer *player = new QMediaPlayer;
+    QVideoWidget *videoWidget = ui->video_ns;
+
+    videoWidget->show();
+
+    player->setSource(QUrl::fromLocalFile("E:\\DBVideo\\NonStd\\Left\\1\\1_1_NS_L.mp4"));
+    QFile file("E:\\DBVideo\\NonStd\\Left\\1\\1_1_NS_L.mp4");
+        if(!file.open(QIODevice::ReadOnly))
+            qDebug() << "Could not open file";
+
+    player->setVideoOutput(videoWidget);
+    player->play();
+
+    //show advice
+
 
 }
 
